41b255409dc01248ebb9fddf8cb9ad9f30e31d8d
Markus Zevnik <marko.zevnik@rettro.si>
2019-06-24 15:31:52 +0200

    json visualizer updated; version bump

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
- * Datafund Consent generator & viewer 
- * Licensed under the MIT license 
- * Created by Markus Zevnik, Tadej Fius, Èrt Ahlin 
+ * Datafund Consent generator & viewer
+ * Licensed under the MIT license
+ * Created by Markus Zevnik, Tadej Fius, ï¿½rt Ahlin
  */
 
 import React from 'react';
@@ -11,7 +11,70 @@ import { ConsentViewer } from "./lib";
 const App = () => (
     <div style={{ width: 640, margin: "15px auto" }}>
         <h1>CR Viewer</h1>
-        <ConsentViewer data={{"test": "lorem ipsum"}} />
+        <ConsentViewer data={{
+            "header": {
+                "alg": "HS256",
+                "typ": "JWT"
+            },
+            "payload": {
+                "version": "Version",
+                "jurisdiction": "test",
+                "consentTimestamp": 213456789,
+                "collectionMethod": "Collection Method",
+                "consentReceiptID": "25efb12c-20fd-4d26-a9fe-f3423a6426f5",
+                "publicKey": "",
+                "language": "English",
+                "piiPrincipalId": "PII PrincipalId",
+                "piiControllers": [
+                    {
+                        "piiController": "PII Controller",
+                        "contact": "Contact",
+                        "address": {},
+                        "email": "info@contact.com",
+                        "phone": "43567890",
+                        "piiControllerUrl": "https://www.aysudhfsgl.com"
+                    },
+                    {
+                        "piiController": "PII Controller2",
+                        "contact": "Contact2",
+                        "address": {},
+                        "email": "info@contact2.com",
+                        "phone": "22222222222",
+                        "piiControllerUrl": "https://www.aysudhfsgl2222.com"
+                    }
+                ],
+                "policyUrl": "https://www.aysudhfsgl.com/policy",
+                "services": [
+                    {
+                        "service": "test",
+                        "purposes": [
+                            {
+                                "purpose": "Purpose1",
+                                "consentType": "Consent Type1",
+                                "purposeCategory": [
+                                    "cat1",
+                                    "cat2"
+                                ],
+                                "piiCategory": [
+                                    "PII Category"
+                                ],
+                                "primaryPurpose": true,
+                                "termination": "wqer"
+                            }
+                        ]
+                    }
+                ],
+                "token": {
+                    "issuer": "issuer",
+                    "subject": "subject",
+                    "audience": "audience",
+                    "expiresIn": "12h",
+                    "algorithm": "RS256"
+                },
+                "iat": 1561377355
+            },
+            "signature": "K1pNwtiAfXRFclrX1s1-NkX-1iICJmsPKbmVUbVgaYg"
+        }} />
     </div>
 );
 
--- src/lib/ConsentViewer.js
+++ src/lib/ConsentViewer.js
@@ -1,29 +1,85 @@
 /**
- * Datafund Consent generator & viewer 
- * Licensed under the MIT license 
- * Created by Markus Zevnik, Tadej Fius, Èrt Ahlin 
+ * Datafund Consent generator & viewer
+ * Licensed under the MIT license
+ * Created by Markus Zevnik, Tadej Fius, ï¿½rt Ahlin
  */
 
-import React, { Component } from "react";
-import {JsonTable} from 'react-json-to-html';
+import React, {Component} from "react";
 import _ from "lodash";
+import $ from 'jquery';
 import "./ConsentViewer.css";
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
 
 export default class ConsentViewer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: this.props.data ? this.props.data : {}
-        }
+            data: this.props.data ? this.props.data : {},
+            htmlData: "",
+        };
 
-        console.log(this.props);
-        console.log(props);
+        this.objectToTable = this.objectToTable.bind(this);
+        this.renderHtmlTable = this.renderHtmlTable.bind(this);
+    }
+
+    componentDidMount() {
+        //this.objectToTable(this.state.data);
+        this.renderHtmlTable();
     }
 
     componentWillReceiveProps(nextProps, nextContext) {
         this.setState({
             data: nextProps.data
-        })
+        });
+
+        this.renderHtmlTable();
+    }
+
+    objectToTable(obj) {
+        let table = $('<table>').append('<tbody>');
+        for (let key in obj) {
+            let tr = $('<tr>');
+            let th = $('<th>').append(key);
+            let td = $('<td>');
+            let value;
+            if (typeof (obj[key]) == 'object') {
+                if (obj[key]) {
+                    value = this.objectToTable(obj[key]);
+                } else {
+                    value = '<span class="null">null</span>';
+                }
+            } else if (typeof (obj[key]) == 'boolean') {
+                let str = (obj[key]) ? 'true' : 'false';
+                value = '<span class="boolean">' + str + '</span>';
+            } else if (typeof (obj[key]) == 'string') {
+                value = '<span class="string">"' + obj[key] + '"</span>';
+            } else {
+                value = obj[key].valueOf();
+            }
+            td.append(value);
+            tr.append(th).append(td);
+            table.append(tr);
+        }
+        return table;
+    }
+
+
+    renderHtmlTable() {
+        const _this = this;
+
+        let value = "";
+
+        try {
+            value = _this.objectToTable(this.state.data);
+        } catch (e) {
+            value = '<pre>' + e + '</pre>';
+        }
+
+        _this.setState({htmlData: $(value).html()})
+
     }
 
 
@@ -31,12 +87,18 @@ export default class ConsentViewer extends Component {
 
         return (
             <div className="consentViewerContainer">
-                {!_.isEmpty(this.state.data) &&
-                <JsonTable json={this.state.data}/>
+
+
+                {this.state.htmlData &&
+                <div className="table-responsive">
+                    <table className="table" id="container"
+                           dangerouslySetInnerHTML={{__html: this.state.htmlData}}></table>
+                </div>
                 }
 
+
                 {_.isEmpty(this.state.data) &&
-                    <em>Cosent Recepeit data not available.</em>
+                <em>Cosent Recepeit data not available.</em>
                 }
             </div>
         )
